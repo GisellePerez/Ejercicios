17bb5a1d4247aa78d7834381fc70f3d4e816c04c
GisellePerez <giselleperez91@gmail.com>
2018-05-09 19:43:39 -0300

    continuo form jquery

--- modulo3/jQuery/Ejercicios-jQuery/js/index.js
+++ modulo3/jQuery/Ejercicios-jQuery/js/index.js
@@ -1,4 +1,5 @@
 var docList = ['Documento Único','Libreta de Enrolamiento', 'Libreta Cívica'];
+var docNum = $('#docNum').val();
 
 function loadDocType(){
 
@@ -12,8 +13,9 @@ function loadDocType(){
 function requiredFields(){
     let required = $('.obligatorio');
     let obLabels =$('.obLabel');
-    //console.log(required); 
+    //console.log(required);
 
+    // Versión con FOR
     for(var i = 0 ; i < required.length ; i++){
 
         if(required[i].value == 0 || required[i].value == '0'){
@@ -21,44 +23,76 @@ function requiredFields(){
         }else{
             obLabels[i].innerHTML = ' ';
         }
-    }
-
+    } 
 }
 
-function validate(){
+function getAge(){
+    let birthday=$('#birthday').val(); //lo toma así: aaaa-mm-dd 
+    
+    //valores del naimiento del usuario
+    let values = birthday.split("-");
+    let birthDay = values[2];
+    let birthMonth = values[1];
+    let birthYear = values[0]; //solo el año
+   
+    //Ahora los valores actuales
+    let currentDate = new Date(); //fecha actual
+    let currentYear = currentDate.getFullYear(); //año actual
+   
+    let age = currentYear - birthYear; //console.log('Tu edad es '+age);
+    if(birthday){
+        if(age <= 0){
+            $('#errorNac').innerHTML='Edad inválida'; 
+            console.log('Edad inválida');
+        }else if (age < 18){
+            $('#errorNac').innerHTML='Eres demasiado joven'; 
+            console.log('Eres demasiado joven');
+        }else{
+            $('#errorNac').innerHTML= ' ';
+            console.log('Edad correcta');
+        }
+    }else{
+        return false;
+    }
+    //funciona pero no aparecen las labes otra vez. El console.log sí lo toma
+}
 
-    console.log('hola');
+function valDoc(docNum){
 
-   // function validateName(){
+    if(/^[1-9][0-9]{8}$/.test(docNum)){ //siempre tira error
+        console.log('documento válido')
+        return true; 
+    } else {
+        console.log('documento NO válido')
+       return false;  
+    }    
+}
 
-        var name = $('#name').val();
-        var surname = $('#lastName').val();
-        let error = $('.error');
-        console.log(name);
-        console.log(surname);
-
-        /*
-        if(name != ' '){
-            name.keyup(function(){
-
-                var resultado = $( this ).val();
-                var total = resultado.length;
-
-                if(total < '50'){
-                //$( "p" ).text("Hay un total de "+total+" carácteres ");
-                    error.innerHTML('NOMBRE DEMASIADO LARGO');
-                }
-        } */
-  //  }
-}   
-        
+function clearFields(){
+    var inputsText = $('[type=text]').val();
+    console.log(inputsText);
+    if(inputsText != 0 || inputsText != ' '){
+        inputs.val() = ' ';
+    }
+}
 
 
 loadDocType();
 
+/* no funciona :(
+$(document).on("keypress", '#btn', function(e) {
+    if (e.keyCode == 13) {
+        requiredFields();
+    }
+  });
+  */
 
 $('#btn').click(function(){
     requiredFields();
-    validate();
+    //validate();
+    getAge();
+    valDoc();
+    clearFields();
 }); 
+
 
